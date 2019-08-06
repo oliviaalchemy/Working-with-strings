728d9b11ade27497ab82ac847ec5e04ecf092326
oliviaalchemy <olivia@alchemytechs.com>
2019-08-06 13:47:45 +0100

    Add project files.

---
+++ Working with strings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Working_with_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            string phrase = "Giraffe Academy";
+            Console.WriteLine( phrase.Substring(8, 3) );  //start at 8 and use three characters altogether eg. Aca
+
+            Console.ReadLine();
+            
+        }
+    }
+}
